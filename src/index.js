1f8e0a6e99fe65979b723f924bd2567bcef39754
YOUR NAME <solisamo6@gmail.com>
2019-11-14 12:37:46 -0600

    Change the server
    Change the server and the routes.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const express = require('express')
+const morgan  = require('morgan')
+const path = require('path')
 const app = express()
 
 //Settings
-
+app.set('port', process.env.PORT || 3000)
 
 //Middlewares
-
+app.use(morgan('dev'))
+app.use(express.json())
 
 //Routes
-
+app.use('/api/task', require('./routes/task.routes'))
 
 //Static files
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Start server
-app.listen(3000, () =>{
-  console.log('Server on port 3000')
+app.listen(app.get('port'), () =>{
+  console.log(`Server on port ${app.get('port')}`)
 })
