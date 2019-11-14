f7cd488d502e774dae4a4f17c589fe947f204a47
YOUR NAME <solisamo6@gmail.com>
2019-11-14 12:07:13 -0600

    Initial commit
    create the initial folders and structure

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+//Settings
+
+
+//Middlewares
+
+
+//Routes
+
+
+//Static files
+
+
+//Start server
+app.listen(3000, () =>{
+  console.log('Server on port 3000')
+})
